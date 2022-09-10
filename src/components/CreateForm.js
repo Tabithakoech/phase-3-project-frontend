f8595861d3f29c1cc1b34c4bf7abef4ba480d279
TabithaKoech <tabbyk1718@gmail.com>
2022-09-10 20:41:59 +0300

    Created a form with a with an onchage eveny on each form data and a submit button

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -18,40 +18,40 @@ function CreateForm({postEmployee, departments}){
         e.preventDefault()
         postEmployee(formData)
     } 
-//     return(
-//         <div className='employee_form'>
-//             <h1 className='title'>Employee Database</h1>
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                 Name:
-//                 <input type="text" name="name" value={formData.name} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Age:
-//                 <input type="number" name="age" value={formData.age} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Gender:
-//                 <input type="text" name="gender" value={formData.gender} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Phone number:
-//                 <input type="text" name="phone" value={formData.phone} onChange={handleChange}/>
-//                 </label>
-//                 <label>
-//                 Department:
-//                 <input type="text" name="department_id" value={formData.department_id} onChange={handleChange}/>
+    return(
+        <div className='employee_form'>
+            <h1 className='title'>Employee Database</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                Name:
+                <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+                </label>
+                <label>
+                Age:
+                <input type="number" name="age" value={formData.age} onChange={handleChange}/>
+                </label>
+                <label>
+                Gender:
+                <input type="text" name="gender" value={formData.gender} onChange={handleChange}/>
+                </label>
+                <label>
+                Phone number:
+                <input type="text" name="phone" value={formData.phone} onChange={handleChange}/>
+                </label>
+                <label>
+                Department:
+                <input type="text" name="department_id" value={formData.department_id} onChange={handleChange}/>
 
-//                 {/* <select name="department_id" onChange={handleChange}>
-//                     <option>Select A Department</option>
-//                     {departments.map(s => <option value={s.id}>{s.department_name}</option>)}
-//                 </select> */}
-//                 </label>
-//                 <input className='submit_btn' type="submit" value="Submit" />
-//             </form>
-//         </div>
+                {/* <select name="department_id" onChange={handleChange}>
+                    <option>Select A Department</option>
+                    {departments.map(s => <option value={s.id}>{s.department_name}</option>)}
+                </select> */}
+                </label>
+                <input className='submit_btn' type="submit" value="Submit" />
+            </form>
+        </div>
         
-//     )
-// }
+    )
+}
 
-// export default CreateForm
+export default CreateForm
